2eccf31027a2821e6484eab9e3b2bf38119bf9cf
liu <liupanfengfreedom@gmail.com>
2020-01-10 13:55:14 +0800

    primary

---
+++ Server/Program.cs
@@ -0,0 +1,186 @@
+﻿///////////////match server ok
+///copyright***
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MatchServer
+{
+    class TCPClienttype
+    {
+        public  string map;
+        public string mapID;
+        public string vip;
+        public string rank;
+        public string nvn;
+        public  HashSet<TCPClient> MatchHashsetPool;
+        public Room currentroom=null;
+        public TCPClienttype()
+        {
+            map = null;
+            vip = null;
+            rank = null;
+            MatchHashsetPool = new HashSet<TCPClient>();
+        }
+    }
+    class Program
+    {
+        private static readonly object matchLock = new object();
+        private static readonly object singinLock = new object();
+        static List<TCPClienttype> AllWaitforMatchpools = new List<TCPClienttype>();
+        static List<TCPClient> singinpool = new List<TCPClient>();
+        static List<Room> roomlist = new List<Room>();
+
+        static void Main(string[] args)
+        {
+            IPAddress ipAd = IPAddress.Parse("192.168.1.240");
+            TcpListener myList = new TcpListener(ipAd, 8001);
+            /* Start Listeneting at the specified port */
+            myList.Start();
+            Thread matchalgorithm = new Thread(new ThreadStart(Matchalgorithm));
+            Thread matchalhelp = new Thread(new ThreadStart(singinpooltomatchpool));
+            matchalhelp.Start();
+            matchalgorithm.Start();
+            while (true)
+            {
+                Socket st = myList.AcceptSocket();
+                TCPClient tcpClient = new TCPClient(st);
+                lock (singinLock)
+                {
+                    singinpool.Add(tcpClient);
+                }
+                int len = singinpool.Count;    
+                Console.WriteLine("singinpool " + len.ToString());
+            }
+        }
+        static void Matchalgorithm()
+        {
+            while (true)
+            {
+                lock (matchLock)
+                {
+                    for (int j = 0; j < AllWaitforMatchpools.Count; j++)
+                    {
+                        int len = AllWaitforMatchpools[j].MatchHashsetPool.Count;
+                        // Console.WriteLine("for ahead AllWaitforMatchpools len" + len.ToString());
+                        for (int i = 0; i < len; i++)
+                        {
+                            //  Console.WriteLine("AllWaitforMatchpools len"+ len.ToString());
+
+                            if (AllWaitforMatchpools[j].currentroom == null || AllWaitforMatchpools[j].currentroom.mprocess.HasExited)
+                            {
+                                int nvn=0;
+                                if (!Int32.TryParse(AllWaitforMatchpools[j].nvn, out nvn))
+                                {
+                                    nvn = 2;
+                                }
+                                AllWaitforMatchpools[j].currentroom = new Room(nvn, LanchServer.CreateOneRoom());//the client who create room determine the nvn 
+                                AllWaitforMatchpools[j].currentroom.listroom = roomlist;
+                                AllWaitforMatchpools[j].currentroom.tcpclienttype = AllWaitforMatchpools[j];
+                               //Thread.Sleep(100);//wait IP port take effect
+                            }
+                            TCPClient temp = AllWaitforMatchpools[j].MatchHashsetPool.ElementAt(i); ;
+                            int maxnumber = AllWaitforMatchpools[j].currentroom.getroommaxpeoplenumber();
+                            int currentnumber = AllWaitforMatchpools[j].currentroom.getnumberofpeopleinroom();
+                            if (currentnumber < maxnumber)
+                            {
+                                AllWaitforMatchpools[j].currentroom.Add(temp);
+                            }
+                            else//full
+                            {
+                                FMessagePackage mp = new FMessagePackage();
+                                mp.MT = MessageType.MAPISFULL;
+                                String str = JsonConvert.SerializeObject(mp);
+                                temp.Send(str);// tell client the number of people in current map has reach the ceilling
+                            }
+
+                            temp.isinmatchpool = true;
+                            AllWaitforMatchpools[j].MatchHashsetPool.Remove(temp);
+                            len = AllWaitforMatchpools[j].MatchHashsetPool.Count;
+                            i = 0;
+                            //if (!notfull)
+                            //{
+                            //    roomlist.Add(AllWaitforMatchpools[j].currentroom);
+                            //    AllWaitforMatchpools[j].currentroom = null;
+                            //}
+                        }
+
+                    }
+ 
+                }
+                Thread.Sleep(200);
+            }
+        }
+        static void singinpooltomatchpool()
+        {
+            while (true)
+            {
+                lock (matchLock)
+                {
+                    int len = 0;
+                    lock (singinLock)
+                    {
+                         len = singinpool.Count;
+                        for (int i = 0; i < len; i++)
+                        {
+                            if (singinpool[i].mclosed)//clear offline client
+                            {
+                                singinpool.RemoveAt(i);
+
+                                len = singinpool.Count;
+                                i = 0;
+                            }
+                        }
+                    }
+
+                    len = singinpool.Count;
+                    for (int i = 0; i < len; i++)
+                    {
+                        if (singinpool[i].isinmatchpool)
+                        {
+                            continue;
+                        }
+                        //singinpool[i].isinmatchpool = true;
+                        bool b_hasmap = false;
+                        for (int j=0 ; j < AllWaitforMatchpools.Count; j++)
+                        {
+                            bool b = AllWaitforMatchpools[j].map == singinpool[i].map && !String.IsNullOrEmpty(singinpool[i].map);
+                            bool b1 = AllWaitforMatchpools[j].rank == singinpool[i].rank;
+                            b1 = true;
+                            bool b2 = AllWaitforMatchpools[j].vip  == singinpool[i].vip;
+                            b2 = true;
+                            bool b3 = AllWaitforMatchpools[j].nvn == singinpool[i].nvn;
+                            bool b4 = AllWaitforMatchpools[j].mapID == singinpool[i].mapID;
+                            if (b&&b1&&b2 && b3 && b4)
+                            {
+                                b_hasmap = true;
+                                AllWaitforMatchpools[j].MatchHashsetPool.Add(singinpool[i]);
+                                Console.WriteLine(" if (b&&b1&&b2)");
+
+                            }
+                        }
+                        if (!b_hasmap)
+                        {
+                            Console.WriteLine("if (!b_hasmap)");
+                            TCPClienttype temptype = new TCPClienttype();
+                            temptype.map = singinpool[i].map;
+                            temptype.rank = singinpool[i].rank;
+                            temptype.vip = singinpool[i].vip;
+                            temptype.nvn = singinpool[i].nvn;
+                            temptype.mapID = singinpool[i].mapID;
+                            AllWaitforMatchpools.Add(temptype);//
+                            ///remove some TCPClienttype from AllWaitforMatchpools maybe not necessary;
+                        }
+                    }
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ Server/TcpClient.cs
@@ -0,0 +1,164 @@
+﻿#define UTF16
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Threading;
+namespace MatchServer
+{
+   // public delegate void OnReceivedCompleted(List<byte>mcontent);
+    public delegate void OnReceivedCompleted(byte[] buffer);
+ class TCPClient
+    {
+        public String map { private set; get; }
+        public String mapID { private set; get; }
+        public String vip { private set; get; }
+        public String rank { private set; get; }
+        public String nvn { private set; get; }
+
+        /// <summary>
+        /// //////////////////////////////////////////////
+        /// </summary>
+        public  Room room;
+        public bool isinmatchpool=false;
+        public bool mclosed = false;
+        Socket clientsocket;
+        public OnReceivedCompleted OnReceivedCompletePointer=null;
+        bool entrymapok;
+        const int BUFFER_SIZE = 65536;
+        public byte[] receivebuffer = new byte[BUFFER_SIZE];
+        string filestringpayload;
+        bool isfile = false;
+        Thread ReceiveThread;
+        public bool getentrymapisok() {
+            return entrymapok;
+        }
+        public TCPClient(Socket msocket)
+        {
+            Console.WriteLine("TCPClient "+ msocket.RemoteEndPoint);
+            entrymapok = false;
+            clientsocket = msocket;
+            OnReceivedCompletePointer += messagehandler;
+            ReceiveThread = new Thread(new ThreadStart(ReceiveLoop));
+            ReceiveThread.IsBackground = true;
+            ReceiveThread.Start();      
+        }
+        ~TCPClient()
+        {
+            Console.WriteLine("TCPClient In destructor.");
+        }
+        public void Send(byte[] buffer)
+        {
+            if (clientsocket != null)
+            {
+                clientsocket.Send(buffer);
+            }
+        }
+        public void Send(String message)
+        {
+#if UTF16
+            UnicodeEncoding asen = new UnicodeEncoding();
+#else
+            ASCIIEncoding asen = new ASCIIEncoding();
+#endif
+            if (clientsocket != null)
+            {
+                clientsocket.Send(asen.GetBytes(message));
+            }
+        }
+        void ReceiveLoop()
+        {
+            while (true)
+            {
+                try {
+                    Array.Clear(receivebuffer, 0, receivebuffer.Length);
+                    clientsocket.Receive(receivebuffer);
+                    OnReceivedCompletePointer?.Invoke(receivebuffer);
+                    Thread.Sleep(30);
+                }
+                catch (SocketException)
+                {
+                    mclosed = true;
+                    CloseSocket();
+                    room?.Remove(this);
+                    ReceiveThread.Abort();
+                }
+            }
+
+        }
+        public void CloseSocket()
+        {
+            clientsocket.Close();
+        }
+        void messagehandler(byte[] buffer)
+        {
+            FMessagePackage mp;
+            try
+            {
+#if UTF16
+                var str = System.Text.Encoding.Unicode.GetString(buffer);
+#else
+            var str = System.Text.Encoding.UTF8.GetString(buffer);
+#endif
+                int len = str.Length;
+                string filestr = "{\r\n\t\"mT\": \"FILE";
+                string fileendstr = "{\r\n\t\"mT\": \"FILEEND";
+                if (isfile)
+                {
+                    if (str.StartsWith(fileendstr))
+                    {
+                        int size = filestringpayload.Length;
+                        isfile = false;
+                        return;
+                    }
+
+                    filestringpayload += str;
+                    int size1 = filestringpayload.Length;
+                    FMessagePackage filesend = new FMessagePackage();
+                    filesend.MT = MessageType.FILE;//go on             
+                    String strsend = JsonConvert.SerializeObject(filesend);
+                    Send(strsend);
+                    return;
+                }
+                if (str.StartsWith(filestr))
+                {
+                    isfile = true;
+                    return;
+                }
+
+                mp = JsonConvert.DeserializeObject<FMessagePackage>(str);
+                switch (mp.MT)
+                {
+                    case MessageType.MATCH:
+                        String[] strarray = mp.PayLoad.Split('?');
+                        map = strarray[0];//map
+                        mapID = strarray[1];//mapID
+                        nvn = strarray[2];//nvn
+                        Console.WriteLine(map);
+                        break;
+                    case MessageType.EntryMAPOK:
+                        entrymapok = true;
+                        break;
+                    case MessageType.EXITGAME:
+                        mclosed = true;
+                        CloseSocket();
+                        room?.Remove(this);
+                        ReceiveThread.Abort();
+                        break;
+                }
+
+            }
+            catch(Newtonsoft.Json.JsonSerializationException){//buffer all zero//occur when mobile client force kill the game client
+                mclosed = true;
+                CloseSocket();
+                room?.Remove(this);
+                ReceiveThread.Abort();
+            }
+
+
+        }
+    }
+}
